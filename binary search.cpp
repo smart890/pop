a356fd18e20445feed1113e5fdf7c6b206778e2f
smart890 <130353545+smart890@users.noreply.github.com>
2023-04-10 14:19:27 +0530

    Add files via upload

---
+++ binary search.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+int n,i,low,high,mid,flag=0;
+int key,a[50];
+printf("\n Enter the number of elements:\n");
+scanf("%d",&n);
+printf("\nEnter the elements of array :\n");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("\nEnter the key to be searched:\n");
+scanf("%d",&key);
+low=0;
+high=n-1;
+while(low<=high)
+{
+mid=(low+high)/2;
+if(key==a[mid])
+{
+printf("\nkey is found at location %d\n",mid+1);
+flag=1;
+break;
+}
+else if(key>a[mid])
+{
+low=mid+1;
+}
+else
+{
+high=mid-1;
+}
+}
+if(flag==0)
+{
+printf("\n key not found in the list \n");
+}
+}
